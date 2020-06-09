a90bde9f072a2bc12e8be74e153893af4e44e320
Philipe Lopes <philipe.lopes07@gmail.com>
2020-06-09 02:55:47 -0300

    add creds and README file

--- src/config/contants.ts
+++
@@ -1,8 +0,0 @@
-const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyC9Akx1Mlf1qh6qVlWtzkNb-nCnziEgjA0",
-    authDomain: "ionic-chat-302d0.firebaseapp.com",
-    databaseURL:"https://ionic-chat-302d0.firebaseio.com",
-    projectId: "ionic-chat-302d0",
-    storageBucket:"ionic-chat-302d0.appspot.com",
-    messagingSenderId:"873760497639"
-}
